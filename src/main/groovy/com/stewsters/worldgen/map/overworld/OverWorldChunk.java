c4b54e2632a66f265ccacfd17e63843a3f0dec1a
Adrian <stewsters@gmail.com>
2016-09-28 22:10:26 -0500

    Extracing biome selection

--- src/main/groovy/com/stewsters/worldgen/map/overworld/OverWorldChunk.java
+++ src/main/groovy/com/stewsters/worldgen/map/overworld/OverWorldChunk.java
@@ -77,58 +77,9 @@ public class OverWorldChunk {
 
         if (river[pX][pY])
             return RIVER;
-        return biome(elevation[pX][pY], temperature[pX][pY], precipitation[pX][pY]);
+        return BiomeType.biome(elevation[pX][pY], temperature[pX][pY], precipitation[pX][pY]);
     }
 
 
-    // Tree Line - highest survivable trees
-    // 4000 near the equator, 2000 near the poles
 
-    //timberline - Highest canopy - forest
-
-    //   Simplified biome chart: http://imgur.com/kM8b5Zq
-    private BiomeType biome(double e, double t, double p) {
-
-        if (e < 0.0 && t < 0.0) return SEA_ICE;
-
-        if (e < -0.75) return OCEAN_ABYSSAL;
-        if (e < -0.05) return OCEAN_DEEP;
-        if (e < 0.0) return OCEAN_SHALLOW;
-
-        if (t < 0) return SNOW;
-        if (e > 0.7) { // Above Treeline
-            return BARE;
-        }
-
-        if (e < 0.01) {
-            return BEACH;
-        }
-
-        if (t < 0) {
-            if (p < 0.1) return SCORCHED;
-            if (p < 0.2) return BARE;
-            if (p < 0.5) return TUNDRA;
-            return SNOW;
-        }
-
-        if (t < 0.40) {
-            if (p < 0.33) return TEMPERATE_DESERT;
-            if (p < 0.66) return SHRUBLAND;
-            return TAIGA;
-        }
-
-        if (t < 0.6) {
-            if (p < 0.16) return TEMPERATE_DESERT;
-            if (p < 0.50) return GRASSLAND;
-            if (p < 0.83) return TEMPERATE_DECIDUOUS_FOREST;
-            return TEMPERATE_RAIN_FOREST;
-        }
-
-        if (p < 0.10) return SUBTROPICAL_DESERT;
-        if (p < 0.33) return GRASSLAND;
-        if (p < 0.66) return TROPICAL_SEASONAL_FOREST;
-        return TROPICAL_RAIN_FOREST;
-
-
-    }
 }
